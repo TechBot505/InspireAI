4e3db69f65fc327550a45c42c8f0988781de054a
Rohit <rohitdhanotia2003@gmail.com>
2024-08-21 00:44:13 +0530

    Header Fix

--- app/dashboard/_components/SearchSection.tsx
+++ app/dashboard/_components/SearchSection.tsx
@@ -4,10 +4,10 @@ import React from "react";
 function SearchSection({ onSearchInput }: any) {
   return (
     <div className="p-10 bg-gradient-to-br from-purple-500 via-purple-700 to-blue-500 flex flex-col justify-center items-center text-white">
-      <h2 className="text-3xl font-bold">Browse all Templates</h2>
-      <p>What would you like to create today?</p>
+      <h2 className="text-3xl font-bold text-center">Browse all Templates</h2>
+      <p className="pt-2">What would you like to create today?</p>
       <div className="w-full flex justify-center">
-        <div className="flex gap-2 items-center p-2 border rounded-md bg-white my-5 w-[70%] sm:w-[50%]">
+        <div className="flex gap-2 items-center p-2 border rounded-md bg-white my-5 w-[80%] sm:w-[60%]">
           <Search className="text-primary" />
           <input
             type="text"
